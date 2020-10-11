4f8b370d6c12b984272197a5992e8ce2ae40ef94
Matthew Kornfield <mckornfield@gmail.com>
2020-10-11 19:09:08 +0000

    remove relative dir

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-ls ../mc-live-server/minecraft_server*.jar
-java -Xmx1024M -Xms1024M -jar ../mc-live-server/minecraft_server*.jar nogui
+java -Xmx1024M -Xms1024M -jar minecraft_server*.jar nogui
