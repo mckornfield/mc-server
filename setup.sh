5dcd425279a8898ca753509c4dd103489aaa1be4
mckornfield <matthew.kornfield@appian.com>
2020-10-11 11:48:28 -0700

    fix dir bugs

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 apt-get update
 apt-get install openjdk-8-jdk
 mkdir -p ../mc-live-server
-pushd mc-live-server
-curl -o minecraft_server.1.16.3.jar https://launcher.mojang.com/v1/objects/f02f4473dbf152c23d7d484952121db0b36698cb/server.jar
+pushd ../mc-live-server
+curl -o /opt/mc-live-server/minecraft_server.1.16.3.jar https://launcher.mojang.com/v1/objects/f02f4473dbf152c23d7d484952121db0b36698cb/server.jar
 popd
--- setup_remote.sh
+++ setup_remote.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 host=$(cat ${PWD}/hosts)
-ssh -A root@$1 "export GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no' && mkdir -p /opt/ && cd /opt && [ -d /opt/mc-server ] && git -C mc-server pull ||  git clone git@github.com:mckornfield/mc-server"
-ssh -A root@$1 "/opt/mc-server/setup.sh"
+ssh -A root@${host} "export GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no' && mkdir -p /opt/ && cd /opt && [ -d "/opt/mc-server" ] && git -C mc-server pull ||  git clone git@github.com:mckornfield/mc-server"
+ssh -A root@${host} "/opt/mc-server/setup.sh"
