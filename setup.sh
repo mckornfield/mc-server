c3a31ce727bb4a5d82635caf3be57cf5cb10f460
mckornfield <matthew.kornfield@appian.com>
2020-10-13 23:33:52 -0700

    bugsss

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 apt-get update
-apt-get install openjdk-8-jdk unzip zip
+apt-get install openjdk-8-jdk unzip zip -y
 live_dir=/opt/mc-live-server
 mkdir -p $live_dir
 ln -s /opt/mc-server/run_server.sh ${live_dir}/run_server.sh
 pushd $live_dir
-echo "eulta=true" > eula.txt
+echo "eula=true" > eula.txt
 curl -o ${live_dir}/minecraft_server.1.16.3.jar https://launcher.mojang.com/v1/objects/f02f4473dbf152c23d7d484952121db0b36698cb/server.jar
