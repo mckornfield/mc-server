cf8c9363f33fd78001b5daa0a7e8f184e3c32f5e
Matt Kornfield <matt.kornfield@appian.com>
2022-06-09 21:21:44 -0400

    upgrade version to 1.19

--- main.sh
+++ main.sh
@@ -4,11 +4,12 @@ terraform init
 terraform apply -auto-approve
 popd
 ./update_host.sh
+host=$(cat ${PWD}/hosts)
+ssh-keyscan -H $host >> ~/.ssh/known_hosts
 sleep 10 # Key propagation time x/
 ./setup_remote.sh
 cp world_saved.zip world.zip
 ./copy_world.sh
-host=$(cat ${PWD}/hosts)
 scp ops.json root@${host}:/opt/mc-live-server/ops.json
 ./start_server_remote.sh
 echo "MC server running on ${host}"
--- setup.sh
+++ setup.sh
@@ -12,4 +12,4 @@ mkdir -p $live_dir
 ln -sf /opt/mc-server/run_server.sh ${live_dir}/run_server.sh
 pushd $live_dir
 echo "eula=true" > eula.txt
-curl -o ${live_dir}/minecraft_server.1.18.2.jar https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar
+curl -o ${live_dir}/minecraft_server.1.19.jar https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar
